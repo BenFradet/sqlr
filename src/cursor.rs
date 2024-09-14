4b009348248c0363cec8c79afd0b80beb50650d4
benfradet <benjamin.fradet@gmail.com>
2024-09-14 23:03:49 +0200

    refactor record field type -> value

--- src/cursor.rs
+++ src/cursor.rs
@@ -1,11 +1,6 @@
 use std::borrow::Cow;
 
-use crate::{
-    page::Page,
-    pager::Pager,
-    utils,
-    value::Value,
-};
+use crate::{page::Page, pager::Pager, utils, value::Value};
 
 #[derive(Debug)]
 pub struct Cursor<'p> {
@@ -38,28 +33,7 @@ impl<'p> Cursor<'p> {
             _ => return None,
         };
 
-        match record_field.field_type {
-            RecordFieldType::Null => Some(Value::Null),
-            RecordFieldType::I8 => Some(Value::Int(read_i8_at(payload, record_field.offset))),
-            RecordFieldType::I16 => Some(Value::Int(read_i16_at(payload, record_field.offset))),
-            RecordFieldType::I24 => Some(Value::Int(read_i24_at(payload, record_field.offset))),
-            RecordFieldType::I32 => Some(Value::Int(read_i32_at(payload, record_field.offset))),
-            RecordFieldType::I48 => Some(Value::Int(read_i48_at(payload, record_field.offset))),
-            RecordFieldType::I64 => Some(Value::Int(read_i64_at(payload, record_field.offset))),
-            RecordFieldType::Float => Some(Value::Float(read_f64_at(payload, record_field.offset))),
-            RecordFieldType::String(length) => {
-                let value = std::str::from_utf8(
-                    &payload[record_field.offset..record_field.offset + length],
-                )
-                .expect("invalid utf8");
-                Some(Value::String(Cow::Borrowed(value)))
-            }
-            RecordFieldType::Blob(length) => {
-                let value = &payload[record_field.offset..record_field.offset + length];
-                Some(Value::Blob(Cow::Borrowed(value)))
-            }
-            _ => panic!("unimplemented"),
-        }
+        record_field.field_type.value(payload, record_field.offset)
     }
 }
 
@@ -91,7 +65,7 @@ fn read_f64_at(input: &[u8], offset: usize) -> f64 {
     f64::from_be_bytes(input[offset..offset + 8].try_into().unwrap())
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub enum RecordFieldType {
     Null,
     I8,
@@ -148,6 +122,29 @@ impl RecordFieldType {
             Self::String(s) => s,
         }
     }
+
+    fn value(self, payload: &[u8], offset: usize) -> Option<Value> {
+        match self {
+            Self::Null => Some(Value::Null),
+            Self::I8 => Some(Value::Int(read_i8_at(payload, offset))),
+            Self::I16 => Some(Value::Int(read_i16_at(payload, offset))),
+            Self::I24 => Some(Value::Int(read_i24_at(payload, offset))),
+            Self::I32 => Some(Value::Int(read_i32_at(payload, offset))),
+            Self::I48 => Some(Value::Int(read_i48_at(payload, offset))),
+            Self::I64 => Some(Value::Int(read_i64_at(payload, offset))),
+            Self::Float => Some(Value::Float(read_f64_at(payload, offset))),
+            Self::Blob(length) => {
+                let value = &payload[offset..offset + length];
+                Some(Value::Blob(Cow::Borrowed(value)))
+            }
+            Self::String(length) => {
+                let value =
+                    std::str::from_utf8(&payload[offset..offset + length]).expect("invalid utf8");
+                Some(Value::String(Cow::Borrowed(value)))
+            }
+            _ => None,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -185,5 +182,58 @@ impl RecordHeader {
         }
 
         Ok(RecordHeader { fields })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_record_field_type_tests() -> () {
+        assert_eq!(RecordFieldType::Null, RecordFieldType::parse(0).unwrap());
+        assert_eq!(RecordFieldType::I8, RecordFieldType::parse(1).unwrap());
+        assert_eq!(RecordFieldType::I16, RecordFieldType::parse(2).unwrap());
+        assert_eq!(RecordFieldType::I24, RecordFieldType::parse(3).unwrap());
+        assert_eq!(RecordFieldType::I32, RecordFieldType::parse(4).unwrap());
+        assert_eq!(RecordFieldType::I48, RecordFieldType::parse(5).unwrap());
+        assert_eq!(RecordFieldType::I64, RecordFieldType::parse(6).unwrap());
+        assert_eq!(RecordFieldType::Float, RecordFieldType::parse(7).unwrap());
+        assert_eq!(RecordFieldType::Zero, RecordFieldType::parse(8).unwrap());
+        assert_eq!(RecordFieldType::One, RecordFieldType::parse(9).unwrap());
+        assert!(RecordFieldType::parse(10).is_err());
+        assert!(RecordFieldType::parse(11).is_err());
+        assert_eq!(
+            RecordFieldType::Blob(0),
+            RecordFieldType::parse(12).unwrap()
+        );
+        assert_eq!(
+            RecordFieldType::Blob(1),
+            RecordFieldType::parse(14).unwrap()
+        );
+        assert_eq!(
+            RecordFieldType::String(0),
+            RecordFieldType::parse(13).unwrap()
+        );
+        assert_eq!(
+            RecordFieldType::String(1),
+            RecordFieldType::parse(15).unwrap()
+        );
+    }
+
+    #[test]
+    fn record_field_type_size_tests() -> () {
+        assert_eq!(RecordFieldType::Null.size(), 0);
+        assert_eq!(RecordFieldType::I8.size(), 1);
+        assert_eq!(RecordFieldType::I16.size(), 2);
+        assert_eq!(RecordFieldType::I24.size(), 3);
+        assert_eq!(RecordFieldType::I32.size(), 4);
+        assert_eq!(RecordFieldType::I48.size(), 6);
+        assert_eq!(RecordFieldType::I64.size(), 8);
+        assert_eq!(RecordFieldType::Float.size(), 8);
+        assert_eq!(RecordFieldType::Zero.size(), 0);
+        assert_eq!(RecordFieldType::One.size(), 0);
+        assert_eq!(RecordFieldType::Blob(0).size(), 0);
+        assert_eq!(RecordFieldType::String(0).size(), 0);
     }
 }
